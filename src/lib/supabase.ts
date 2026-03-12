2fd01b0172c30d84fb332ba7cfb4d7fb63a223e1
Kyaw Khai Soe <kyawkhsoekene@gmail.com>
2026-03-12 16:10:47 +0630

    fix: final authentication and logout logic

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,13 +1,16 @@
-import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
+import { createBrowserClient } from '@supabase/ssr'
 
-export const supabase = createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-        auth: {
-            persistSession: true,
-            storageKey: 'supabase-auth-token',
-            storage: typeof window !== 'undefined' ? window.localStorage : undefined,
+export const createClient = () =>
+    createBrowserClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        {
+            auth: {
+                persistSession: true,
+                autoRefreshToken: true,
+                detectSessionInUrl: true
+            }
         }
-    }
-);
+    )
+
+export const supabase = createClient();
